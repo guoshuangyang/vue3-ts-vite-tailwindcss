2c001c93d869d44416e8130f370be4a507141271
双洋 <221756533@qq.com>
2021-05-10 00:02:29 +0800

    代码初始化

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <div class="w-1/2 mx-auto">
+    <Card>
+      <template #title>
+        <div class="p-2 font-bold">
+          vite + ts + tailwindcss
+        </div>
+      </template>
+      hhhhhhhh
+    </Card>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Card from './components/Card.vue'
+import HelloWorld from './components/HelloWorld.vue'
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld,
+    Card
+  }
+})
+</script>
+
+<style lang="scss">
+#app{
+  background: #efefef;
+}
+</style>
---
+++ src/components/Card.vue
@@ -0,0 +1,16 @@
+<!-- =============================
+ - name: Card.vue
+ - author: yang
+ - description: Card.vue file  vue3组件示例 单文件组件
+ - time: 2021/5/9
+============================= -->
+<template>
+  <div class="w-full bg-white shadow rounded hover:bg-purple-200">
+    <div class="border-b border-gray-300">
+      <slot name="title"></slot>
+    </div>
+    <div class="p-2">
+      <slot></slot>
+    </div>
+  </div>
+</template>
