ae47ad32a456db7447eae8011822ca817aa8c8cf
郭双洋 <guoshuangyang0@163.com>
2022-01-11 13:55:32 +0800

    更新主题配色

--- vite.config.ts
+++ vite.config.ts
@@ -6,12 +6,25 @@ import IconsResolver from 'unplugin-icons/resolver';
 import Icons from 'unplugin-icons/vite';
 import Components from 'unplugin-vue-components/vite';
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+// 在jsx中不不能识别出组件，所以需要按需导入
 import ElementPlus from 'unplugin-element-plus/vite'
-
+import { resolve } from 'path'
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "~/": `${resolve(__dirname, "src")}/`,
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "~/assets/css/element/index.scss" as *;`,
+      },
+    },
+  },
   build: {
     reportCompressedSize: false
   },
@@ -20,7 +33,10 @@ export default defineConfig({
       // options are passed on to @vue/babel-plugin-jsx
     }),
     vue(),
-    ElementPlus(),
+    ElementPlus({
+      // 在tsx（jsx）使用主题
+      useSource: true,
+    }),
     AutoImport({
       imports: ['vue', 'vue-router', 'vuex'],
       resolvers: [ElementPlusResolver()],
@@ -28,6 +44,7 @@ export default defineConfig({
     Components({
       dirs: ['src/components'],
       resolvers: [
+        // 在setup中使用样式（非jsx）
         ElementPlusResolver({ importStyle: 'sass' }),
         IconsResolver(),
       ],
